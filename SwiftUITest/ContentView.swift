2dacb6ef1c233d2418909ac99a06996a5f3daf3c
Changyeol Seo <kongbaguni@gmail.com>
2019-11-06 14:06:08 +0900

    Initial Commit

---
+++ SwiftUITest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITest
+//
+//  Created by Changyul Seo on 2019/11/06.
+//  Copyright © 2019 Changyul Seo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
